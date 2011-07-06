35283d61cbc9a54b42d71d5deba58b40d1b3ba30
Geoffrey Ayap <geoffrey.ayap@98labs.com>
2011-07-06 22:15:38 +0800

    template

---
+++ addons/themes/mariners/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Mariners extends Theme {
+
+    public $name = 'Mariners Theme';
+    public $author = 'geoffrey ayap';
+    public $author_website = '';
+    public $website = '';
+    public $description = 'First template for mariners';
+    public $version = '1.0';
+    
+}
+
+/* End of file theme.php */
