a87024919d2ab733f901e0af3e480e6760cbdb49
Christian <cris@98Labs-Inc-001.(none)>
2011-07-25 20:22:46 +0800

    add new template for the mariners

---
+++ addons/themes/mariners-temp/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Mariners extends Theme {
+
+    public $name = 'Mariners Theme';
+    public $author = 'Christian Danos';
+    public $author_website = '';
+    public $website = '';
+    public $description = 'Second template for mariners';
+    public $version = '1.0';
+    
+}
+
+/* End of file theme.php */
